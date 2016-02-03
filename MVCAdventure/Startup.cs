0e57d6766c21323b8e599e28889f307ca3b3d6c5
javiergandres <javierg.andres@gmail.com>
2016-02-03 16:40:23 +0100

    Creacion MVCAdventure

---
+++ MVCAdventure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAdventure.Startup))]
+namespace MVCAdventure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
