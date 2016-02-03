210cf7e4f68284e83613058f36dd72033dd06e85
Marcos Martínez <marcosmc4_ncs@outlook.es>
2016-02-03 19:49:51 +0100

    Cambios Guardados Equipo C

--- MVCAdventure/Controllers/VentasController.cs
+++ MVCAdventure/Controllers/VentasController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EFAdventure;
 
 namespace MVCAdventure.Controllers
 {
@@ -13,5 +14,28 @@ namespace MVCAdventure.Controllers
         {
             return View();
         }
+
+        public ActionResult RevisarPedido()
+        {
+
+
+            return PartialView("DetallesPedido");
+        }
+
+        private Person GetCliente(int id)
+        {
+            Person cliente = null;
+            using (AdventureWorks2014Entities contexto = new AdventureWorks2014Entities())
+            {
+                var filtro = from ventas in contexto.SalesOrderHeader
+                             join consumidor in contexto.Customer on ventas.CustomerID equals consumidor.CustomerID
+                             join persona in contexto.Person on consumidor.PersonID equals persona.BusinessEntityID
+                             where ventas.SalesOrderID == id
+                             select persona;
+                cliente = filtro.FirstOrDefault();
+            }
+            return cliente;
+        }
+
     }
 }
