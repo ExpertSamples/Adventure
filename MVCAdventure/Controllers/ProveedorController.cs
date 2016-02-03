ef1423c07294d7ad81f84609ba46b9ca5456b483
Ángel Salvatierra Sánchez <angelsalvin@hotmail.com>
2016-02-03 19:19:20 +0100

    Acción GetProovedores en ProveedorController

--- MVCAdventure/Controllers/ProveedorController.cs
+++ MVCAdventure/Controllers/ProveedorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EFAdventure;
 
 namespace MVCAdventure.Controllers
 {
@@ -16,7 +17,24 @@ namespace MVCAdventure.Controllers
 
         public ActionResult GetProveedores()
         {
-            return View("Proveedor");
+            List<Person> ListaProveedores;
+            AdventureWorks2014Entities contexto = new AdventureWorks2014Entities();
+
+            var proveedores = (from prov in contexto.SalesPerson
+                               join per in contexto.Person on prov.BusinessEntityID equals per.BusinessEntityID into lista
+                               from ListaProv in lista
+                               select ListaProv);
+
+            ListaProveedores = proveedores.ToList();
+            contexto.Dispose();
+
+            return View("Proveedor", ListaProveedores);
+        }
+
+        public ActionResult GetProductos()
+        {
+
+            return PartialView("_Productos");
         }
     }
 }
