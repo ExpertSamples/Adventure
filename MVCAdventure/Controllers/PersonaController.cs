2c633738700577ae3044d94e3fcf3c0e2a1812e2
javiergandres <javierg.andres@gmail.com>
2016-02-03 19:19:02 +0100

    Creancion Controlador y vista de Persona

---
+++ MVCAdventure/Controllers/PersonaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFAdventure;
+
+
+namespace MVCAdventure.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult BuscarPersona()
+        {
+
+            ViewBag.BusinessEntityID = 1;
+            return View(BuscarPersona());
+        }
+
+        //TODO Metodo que devuelve los datos de una persona por su ID
+        //Carga de BusinessEntityID
+        public void CargarBusinessEntityID()
+        {
+            
+
+        }
+        
+    }
+}
