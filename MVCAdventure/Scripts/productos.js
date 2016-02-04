fb67a7488fd7a7531747b6ed67d43ec80a2e6841
Sergio Hernández <hernandezbs@live.com>
2016-02-04 19:45:17 +0100

    Cambios javascript productos, cambios en vistas productos, Equipo C

--- MVCAdventure/Scripts/productos.js
+++ MVCAdventure/Scripts/productos.js
@@ -6,13 +6,12 @@ var auxProduct;
 
 $(document).ready(function () {
     $("#proveedores").click(function (e) {
-        var lista = document.getElementById(proveedores);
         var proveedor = e.target;
         currentSalesPerson = proveedor.id;
         $("#" + auxSalesPerson).css("background-color", "#FFFFFF");
         $("#" + currentSalesPerson).css("background-color", "#58ACFA");
         auxSalesPerson = currentSalesPerson;
-        var url = document.location.origin + '/Proveedor/GetProductos?id=' + proveedor.id;
+        var url = document.location.origin + '/Proveedor/GetProductos?provID=' + proveedor.id;
         $.ajax(url).done(function (data) {
             $("#productos").empty();
             $("#productos").html(data);
@@ -20,19 +19,20 @@ $(document).ready(function () {
     });
 });
 
-//CAMBIAR CLICK BOTON
+
 $(document).ready(function () {
     $("#productos").click(function (e) {
         var producto = e.target;
-        var url = document.location.origin + '/Proveedor/ModificarProducto?ProductID=' + producto.id;
-        window.location = url;
+        currentProduct = producto.id;
+        $("#" + auxProduct).css("background-color", "#FFFFFF");
+        $("#" + currentProduct).css("background-color", "#58ACFA");
+        auxProduct = currentProduct;
     });
 });
 
 $(document).ready(function () {
-    $("#productos").click(function (e) {
-        var proveedor = e.target;
-        var url = document.location.origin + '/Proveedor/ModificarProducto?ProductID=' + proveedor.id;
+    $("#modificarProducto").click(function (e) {       
+        var url = document.location.origin + '/Proveedor/ModificarProducto?ProductID=' + currentProduct;
         window.location = url;
     });
 });
