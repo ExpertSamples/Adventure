5d1c15612dfe5d9b423ccf36c2dcd2ec1d6f5293
francisco1582 <al.calleja.plaza@gmail.com>
2016-02-04 17:28:56 +0100

    Queries Controlador Ventas

---
+++ MVCAdventure/Models/VentasDetallesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAdventure.Models
+{
+    public class VentasDetallesModel
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int OrderQty { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal LineTotal { get; set; }
+
+    }
+}
+
+
